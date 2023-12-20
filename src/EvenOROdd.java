ef6ad8e763442fcc16ac7049f3d0e85e1fb739ce
chetana11march <chetana.sadare11march@gmail.com>
2023-12-20 07:43:29 +0530

    SSAG-2202 Even Or Odd Function added

---
+++ src/EvenOROdd.java
@@ -0,0 +1,14 @@
+
+public class EvenOROdd {
+	
+	public static void main(String[] args) {
+		int no=100;
+		
+		if(no%2==0) {
+			System.out.println("Given no is even");
+		}else {
+			System.out.println("Given no is odd");
+		}
+	}
+
+}
