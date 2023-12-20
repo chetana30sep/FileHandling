12ce9c5df4094dd1a15aafe303e0b6958b9247f4
chetana11march <chetana.sadare11march@gmail.com>
2023-12-20 07:23:27 +0530

    Initial Commit

---
+++ src/Addition.java
@@ -0,0 +1,13 @@
+
+public class Addition {
+	
+	public int add(int a,int b) {
+		return a+b;
+	}
+	
+	public static void main(String[] args) {
+		Addition a = new Addition();
+		a.add(10, 20);
+	}
+
+}
